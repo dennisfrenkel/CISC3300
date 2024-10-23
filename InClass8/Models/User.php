b15ab51730c42e7cd6aa93cba8a949783fd24d2f
Dennis F <123222958+dennisfrenkel@users.noreply.github.com>
2024-10-23 16:36:03 -0400

    Upload User.php

---
+++ InClass8/Models/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace models;
+
+class User {
+    public function getAllUsers() {
+        return [
+            ['id' => 1, 'name' => 'John Doe'],
+            ['id' => 2, 'name' => 'Jane Smith'],
+            ['id' => 3, 'name' => 'Alice Johnson']
+        ];
+    }
+}
